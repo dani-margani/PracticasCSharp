ef6d2db6692445f373ef72425fe3baf99ce8aaf2
Daniela <danimargani@gmail.com>
2022-04-19 16:57:16 -0300

    Subo Solucion de CursoFundamentosCSHARP

---
+++ CursoFundamentosCSHARP/CursoFundamentosCSHARP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CursoFundamentosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            sbyte numero = -128;
+            
+            sorry que estoy seria, esta el ceo con cara de orto :)
+                normalmente me reiria
+        }
+    }
+}
