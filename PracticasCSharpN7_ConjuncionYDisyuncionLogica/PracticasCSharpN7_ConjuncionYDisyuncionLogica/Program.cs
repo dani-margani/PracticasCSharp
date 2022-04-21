fc76397173e2feefc3db3766f720b58d6de834cf
Daniela <danimargani@gmail.com>
2022-04-21 11:49:46 -0300

    Practicas C# N7: Conjunción y Distunción.

---
+++ PracticasCSharpN7_ConjuncionYDisyuncionLogica/PracticasCSharpN7_ConjuncionYDisyuncionLogica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasCSharpN7_ConjuncionYDisyuncionLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 25;
+            int hermanos = 0;
+
+            /*if(edad >= 18 && hermanos < 1)
+            {
+                Console.WriteLine("Es mayor de edad y no tiene hermanos");
+            }
+            Console.ReadKey(); 
+            */
+            if (edad >= 18 || hermanos < 1)
+            {
+                Console.WriteLine("Es mayor de edad Ó no tiene hermanos");
+            }
+            Console.ReadKey();
+
+            /*if(edad >= 18) //Ejemplo
+            {
+                if (hermanos < 1)
+                {
+                    // Mayor de edad y no tiene hermanos
+                }
+            }*/
+        }
+    }
+}
