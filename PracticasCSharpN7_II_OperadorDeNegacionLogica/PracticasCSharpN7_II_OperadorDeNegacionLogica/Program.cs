beb75e8a2594a4d369f3bffa7b89e71150711381
Daniela <danimargani@gmail.com>
2022-04-21 15:21:38 -0300

    Practicas C# N7 II: Operador de negacion logica. Pre y post incremento

---
+++ PracticasCSharpN7_II_OperadorDeNegacionLogica/PracticasCSharpN7_II_OperadorDeNegacionLogica/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasCSharpN7_II_OperadorDeNegacionLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 3;
+            //int numero2 = numero + 1;
+
+            int numero2 = ++numero * 2;
+
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero);
+
+
+
+            /*if (!(numero <=5)) //ejemplo de negacion logica
+            {
+                Console.WriteLine("Número es mayor que 5");
+            }
+            */
+
+
+            /* bool variable1 = true;
+            bool variable2 = !variable1; //Esto es un ejemplo previo dejo comentado el codigo para seguir con el ejercicio 
+
+            Console.WriteLine(variable2);
+            */
+
+            Console.ReadKey();
+            
+        }
+    }
+}
