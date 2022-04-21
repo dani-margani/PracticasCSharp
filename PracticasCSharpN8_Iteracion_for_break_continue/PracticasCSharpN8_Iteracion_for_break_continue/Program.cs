a8f3b9c201003e2c941322955dae032ff08e81c4
Daniela <danimargani@gmail.com>
2022-04-21 17:10:39 -0300

    Practicas C# N8: Iteracion, for, break, continue.

---
+++ PracticasCSharpN8_Iteracion_for_break_continue/PracticasCSharpN8_Iteracion_for_break_continue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasCSharpN8_Iteracion_for_break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int tabladel7 = 7;
+
+            for (int elmultiplicador = 0; elmultiplicador <10; elmultiplicador++ )
+            {
+                if(elmultiplicador == 5)
+                {
+                    //break; 
+                    continue;
+                }
+                Console.WriteLine(tabladel7 + " x " + elmultiplicador + " = " + tabladel7 * elmultiplicador);
+            }
+
+            Console.ReadKey();
+        }
+    }
+} 
